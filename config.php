a398512412546b2e458ed3c1043ea6f33ee8a39c
MikeCota20 <miguel.cotagallegos@cesunbc.edu.mx>
2024-11-29 20:23:02 -0800

    depuracion

--- config.php
+++ config.php
@@ -1,12 +1,9 @@
-
 <?php
 // config.php
 define('DB_HOST', '108.179.194.14');
 define('DB_NAME', 'avrgyoco_musicsky');
 define('DB_USER', 'avrgyoco_mike');
 define('DB_PASS', 'mimamixdZ1');
-
-
 
 // Configuración de conexión a la base de datos
 try {
@@ -15,8 +12,3 @@ try {
 } catch (PDOException $e) {
     die("Error en la conexión: " . $e->getMessage());
 }
-?>
-
-
-
-
--- src/controllers/auth.php
+++ src/controllers/auth.php
@@ -2,20 +2,23 @@
 // src/controllers/auth.php
 require_once '../models/User.php';
 
+// Iniciar sesión
 session_start();
 
+// Manejar solicitudes POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['register'])) {
         User::register($_POST['username'], $_POST['password'], $_POST['email']);
         header('Location: /MusicSky/src/views/login.php');
+        exit; // Asegúrate de detener la ejecución
     } elseif (isset($_POST['login'])) {
         $user = User::authenticate($_POST['username'], $_POST['password']);
         if ($user) {
             $_SESSION['user'] = $user;
             header('Location: ../../public/index.php');
+            exit; // Detén la ejecución después de redireccionar
         } else {
             echo "Credenciales incorrectas.";
         }
     }
 }
-?>
